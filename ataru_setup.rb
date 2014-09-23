030d799c87f0c332784464f502b8109a0b7cfaa1
Magdalena <madziaf86@tlen.pl>
2014-09-23 10:43:46 +0200

    run bundle install with ataru in Gemfile and created ataru_setup.rb

---
+++ ataru_setup.rb
@@ -0,0 +1,8 @@
+# "Please write down ALL the dependencies needed to run the code snippets from your Markdown files"
+
+require 'padawans_moon'
+# require gem 1
+# require gem 2
+# require gem 3
+# and so on
+# require ruby standard library (if applicable)
